ebf9029755eb4e50461435f692e2500cabfdd167
thomas.graham <thomas.graham@accenture.com>
2018-09-25 13:57:02 +0100

    Added extent reporting

--- src/test/java/cukeRunners/DefaultRunner.java
+++ src/test/java/cukeRunners/DefaultRunner.java
@@ -1,20 +1,29 @@
 package cukeRunners;
 
+import java.io.File;
+
+import org.junit.AfterClass;
 import org.junit.runner.RunWith;
+
+import com.cucumber.listener.Reporter;
+
 import cucumber.api.junit.Cucumber;
 import cucumber.api.CucumberOptions;
 
-//@RunWith(Cucumber.class)
-//@CucumberOptions(features = "src/test/resources/functionalTests")
-
 @RunWith(Cucumber.class)
 @CucumberOptions(
-		format = {"pretty", "json:target/cucumber.json"},
-		features = "src/test/resources/functionalTests"
-		,glue={"stepDefinitions"}
-		)
-
+        monochrome = true,
+        format = {  "pretty", "html:target/htmlreports" },
+        glue = "stepDefinitions",
+        plugin = { "com.cucumber.listener.ExtentCucumberFormatter:target/cucumber-reports/report.html"}, 
+        features = "src/test/resources/functionalTests/"
+)
 
 public class DefaultRunner {
+	
+	@AfterClass
+    public static void writeExtentReport() {
+        Reporter.loadXMLConfig(new File("config/report.xml"));
 
+}
 }
